c6e90e1d9a34e5cc62c1a144c964a34941cf179d
Amit Rai <arajanrai@gmail.com>
2020-02-15 03:08:56 +0530

    DB connection

--- src/db/db.js
+++ src/db/db.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 //local mysql db connection
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'instagram_clone'
+    user     : 'id12589385_cool94amit',
+    password : 'Rajan@1993',
+    database : 'id12589385_instaclone'
 });
 
 connection.connect(function(err) {
